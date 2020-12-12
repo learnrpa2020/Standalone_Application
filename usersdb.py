51456e9ef8db70cf676ba69c71d1319d64bba46a
Maria Monica <69426325+learnrpa2020@users.noreply.github.com>
2020-12-12 21:26:47 +0530

    done

--- usersdb.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("users.db")
-cur = conn.cursor()
-cur.execute('''INSERT INTO users (
-   	oid,username,password) VALUES (1,"Monica","test1234")
-	  ''')
-conn.commit()
-conn.close()
